37e6140baeac60e9c8d804f92b6fcd24c1e6e814
BenHbrt <benherbert1983@googlemail.com>
2022-06-17 11:25:41 +0200

    Changes to models

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Owner;
+use App\Models\Image;
 
 class Animal extends Model
 {
     use HasFactory;
+
+    public function owners()
+    {
+        return $this->belongsTo(Owner::class);
+    }
+
+    public function images()
+    {
+        return $this->belongsTo(Image::class);
+    }
 }
--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Animal;
 
 class Owner extends Model
 {
     use HasFactory;
+
+    public function animals()
+    {
+        return $this->hasMany(Animal::class);
+    }
 }
